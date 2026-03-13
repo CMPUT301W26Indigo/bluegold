dfb263feab7e90c90a2baa20160ac6494cb19d2f
Jordan Eleniak <jordaneleniak@gmail.com>
2026-03-12 19:49:58 -0600

    Uncommented gradle dependencies. App builds successfully again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,14 +65,14 @@ dependencies {
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:34.10.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 
     // The rest of the Firebase dependencies are from the Figma transfer
     // These files may or may not be necessary.
-//    implementation("com.google.firebase:firebase-auth")
-//    implementation("com.google.firebase:firebase-firestore")
-//    implementation("com.google.firebase:firebase-storage")
-//    implementation("com.google.firebase:firebase-messaging")
-//    implementation(libs.activity)
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-messaging")
+    implementation(libs.activity)
 
     // Testing
     testImplementation(libs.junit)
@@ -83,34 +83,34 @@ dependencies {
     // These may be ones generated from the Figma transfer and may not be
     // necessary.
 
-//    implementation("androidx.core:core-ktx:1.12.0")
-//    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
-//
-//    // Using annotationProcessor for Glide since this is a Java project
-//    implementation("com.github.bumptech.glide:glide:4.16.0")
-//    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-//
-//    implementation("com.google.zxing:core:3.5.2")
-//    implementation("com.google.android.gms:play-services-location:21.1.0")
-//    implementation("com.google.android.gms:play-services-maps:18.2.0")
-//    implementation("com.google.code.gson:gson:2.10.1")
-//    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-//    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-//    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-//    implementation("com.opencsv:opencsv:5.9")
-//    implementation("commons-validator:commons-validator:1.9.0")
-//    implementation("androidx.work:work-runtime-ktx:2.9.0")
-//    implementation("androidx.datastore:datastore-preferences:1.0.0")
-//    implementation("com.jakewharton.timber:timber:5.0.1")
-//
-//    androidTestImplementation("androidx.test:runner:1.5.2")
-//    androidTestImplementation("androidx.test:rules:1.5.0")
-//    testImplementation("io.mockk:mockk:1.13.8")
-//    androidTestImplementation("io.mockk:mockk-android:1.13.8")
-//    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
+    // Using annotationProcessor for Glide since this is a Java project
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    implementation("com.google.zxing:core:3.5.2")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.opencsv:opencsv:5.9")
+    implementation("commons-validator:commons-validator:1.9.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    testImplementation("io.mockk:mockk:1.13.8")
+    androidTestImplementation("io.mockk:mockk-android:1.13.8")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 }
