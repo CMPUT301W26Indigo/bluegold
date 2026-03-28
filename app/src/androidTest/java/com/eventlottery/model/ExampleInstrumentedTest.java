baa1b1bb336540d931571c44d76ec3c1371c1a97
Jordan Eleniak <jordaneleniak@gmail.com>
2026-03-27 18:24:14 -0600

    Refactored test files: migrated test and androidTest package from data.models to model to align with the main app's package convention. Removed com.example.bluegold and data.models. Consolidated duplicate GuestListTest and WaitlistTest files in both data.models and model. Only GuestListTest had tests that weren't the same in either, so made sure to save tests from the duplicate file. WaitlistTest had no differences, and the comment 'MOVED TESTS FROM FIRST FILE' is from a previous PR.

--- app/src/androidTest/java/com/eventlottery/model/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/eventlottery/model/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.bluegold", appContext.getPackageName());
+        assertEquals("com.eventlottery", appContext.getPackageName());
     }
 }
